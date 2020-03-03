9ff93119905e9edea95a3bd838baf37a4de0c053
dataxu <dataxu.mart@gmail.com>
2020-03-03 20:19:57 +0700

    menambahkan view store

---
+++ application/views/store/templates/navbar.php
@@ -0,0 +1,63 @@
+<!-- start navbar -->
+    	<nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background-color: #05b7c2;">
+		  <a class="navbar-brand" href="#">Navbar</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0 font-weight-bolder">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Kategori Produk
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Baju</a>
+                  <a class="dropdown-item" href="#">Jaket</a>
+                  <a class="dropdown-item" href="#">Kosmetik</a>
+                </div>
+              </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Blog</a>
+		      </li>
+		      <form class="form-inline">
+		        	<div class="input-group">
+		                <input type="text" class="form-control bg-light border-0 small" placeholder="cari" aria-label="Search" aria-describedby="basic-addon2">
+		                <div class="input-group-append">
+		                  <button class="btn btn-primary" type="button">
+		                    <i class="fas fa-search fa-sm"></i>
+		                  </button>
+		                </div>
+		            </div>
+			    </form>
+		    </ul>
+
+		  </div>  
+		    <div class="mr-lg-5 my-2 my-lg-0">
+		    	<div>
+		    		
+				    
+				    <span class="fa-layers fa-fw">
+					    <i class="fas fa-shopping-cart fa-2x" style="color:black"></i>
+					    <i class="fas fa-circle fa-inverse fa-2x" data-fa-transform="shrink-5 up-6 right-7" style="color:Tomato"></i>
+					    <span class="fa-layers-text fa-inverse fa-2x" data-fa-transform="shrink-10 up-6 right-10">20</span>
+					</span>
+				    <!--  -->
+				
+		    	</div>
+			    
+		    </div>
+		    <div class="my-2 my-lg-0" style="color:black">
+			    <div>
+			    	<span class="d-none d-lg-inline font-weight-bolder">akun</span>
+			    	<i class="fas fa-user-circle fa-2x"></i>
+			    </div>
+		    </div>
+
+		  
+		</nav>
+
+		<!-- end navbar -->
