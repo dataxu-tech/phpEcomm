d8bb31eb7e1bfed63d227199a9c5187bcfe6484e
dataxu <dataxu.mart@gmail.com>
2020-03-04 21:13:15 +0700

    modifikasi reg

--- application/views/registration.php
+++ application/views/registration.php
@@ -31,22 +31,28 @@
                       <div class="text-center">
                         <h1 class="h4 text-gray-900 mb-4">Buat Akun!</h1>
                       </div>
-                      <form class="user" method="post" action="">
+                      <form class="user" method="post" action=" <?= base_url('auth/registration') ?> ">
                         <div class="form-group">
-                          <input type="text" class="form-control form-control-user" id="username" name="username"placeholder="Username">
+                          <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="username" value="<?= set_value('username'); ?>">
                         </div>
+                        <?= form_error('username', '<small class="text-danger">', '</small>'); ?>
                         <div class="form-group">
-                          <input type="email" class="form-control form-control-user" id="email" name="email" placeholder="Email">
+                          <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email" value="<?= set_value('email'); ?>">
                         </div>
+                        <?= form_error('email', '<small class="text-danger">', '</small>'); ?>
                         <div class="form-group row">
                           <div class="col-sm-6 mb-3 mb-sm-0">
                             <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
                           </div>
+                          <?= form_error('password1', '<small class="text-danger">', '</small>'); ?>
                           <div class="col-sm-6">
                             <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Ulangi Password">
                           </div>
+                          <?= form_error('password2', '<small class="text-danger">', '</small>'); ?>
                         </div>
-                        <button class="btn btn-primary btn-user btn-block">Register</button>
+                        <button type="submit" name="submit" class="btn btn-primary btn-user btn-block">
+                          Buat Akun
+                        </button>                
                       </form>
                       <hr>
                       <div class="text-center">
